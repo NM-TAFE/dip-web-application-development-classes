d2ec87f368a406184bbe39729be1b8de849c0759
Robbozinoz <30688053+Robbozinoz@users.noreply.github.com>
2024-06-25 14:20:43 +0800

    feat:update to class sequence

--- session-02/assets/js_starter/scripts_challenge_4.js
+++ session-02/assets/js_starter/scripts_challenge_4.js
@@ -3,17 +3,9 @@
 let numbers = [1, 2, 3, 4, 5, 6];
 
 // Find the index of the number 5 in the array
-let searchIndex = numbers.findIndex((number) => number === 5)
 
 // If the number 5 is found in the array
-if (searchIndex !== -1) {
-  // Console log each number in the array up to and including 5
-  numbers.forEach((number, index) => {
-    if (index <= searchIndex) {
-      console.log(number)
-    }
-  });
-} else {
-  // Output a message if the number 5 is not in the array
-  console.log("Number not in array")
-}
+
+// Console log each number in the array up to and including 5
+
+// Output a message if the number 5 is not in the array
