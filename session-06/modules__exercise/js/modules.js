ec88cf7cd54b30b30dd68670010a8c747dd9c9fe
Robbo-lab <john.robertson@nmtafe.wa.edu.au>
2024-06-26 16:12:39 +0800

    feat: sculptureList activity

---
+++ session-06/modules__exercise/js/modules.js
@@ -0,0 +1,80 @@
+// scripts_challenge_4.js
+
+// Method to handle the Click Me button click event
+function handleClickMeButton() {
+    const button = document.getElementById('button1');
+    button.addEventListener('click', function() {
+      alert('Button Clicked!');
+    });
+  }
+  
+  // Method to handle the Hover Over Me button hover event
+  function handleHoverButton() {
+    const button = document.getElementById('button2');
+    button.addEventListener('mouseover', function() {
+      alert('Button Hovered Over!');
+    });
+  }
+  
+  // Method to handle the Double Click Me button double click event
+  function handleDoubleClickButton() {
+    const button = document.getElementById('button3');
+    button.addEventListener('dblclick', function() {
+      alert('Button Double Clicked!');
+    });
+  }
+  
+  // Method to add a post to the post container
+  function addPost(title, content) {
+    const postContainer = document.getElementById('postContainer');
+    const post = document.createElement('div');
+    post.className = 'post';
+  
+    const postTitle = document.createElement('h3');
+    postTitle.textContent = title;
+  
+    const postContent = document.createElement('p');
+    postContent.textContent = content;
+  
+    post.appendChild(postTitle);
+    post.appendChild(postContent);
+    postContainer.appendChild(post);
+  }
+  
+  // Method to dynamically add a list of books to the sidebar
+  function addBooks(books) {
+    const sidebar = document.createElement('aside');
+    sidebar.className = 'sidebar';
+  
+    const bookList = document.createElement('ul');
+    bookList.id = 'list';
+  
+    books.forEach(function(book) {
+      const listItem = document.createElement('li');
+      listItem.textContent = book;
+      bookList.appendChild(listItem);
+    });
+  
+    sidebar.appendChild(bookList);
+    const container = document.querySelector('.container');
+    container.appendChild(sidebar);
+  }
+  
+  // Method to initialise the app
+  function initialiseApp() {
+    handleClickMeButton();
+    handleHoverButton();
+    handleDoubleClickButton();
+  
+    // Add some sample posts
+    addPost('First Post', 'This is the content of the first post.');
+    addPost('Second Post', 'This is the content of the second post.');
+  
+    // Add some sample books
+    const books = ['Harry Potter I', 'Harry Potter II', 'Harry Potter III'];
+    addBooks(books);
+  }
+  
+  // Initialise the app when the DOM is fully loaded
+  document.addEventListener('DOMContentLoaded', initialiseApp);
+  
---
+++ session-09/sculptureList/readme.md
@@ -0,0 +1,9 @@
+**Instructions**
+
+- start laragon
+- go to http://localhost/source/repos/dip-web-application-development-classes/session-09/sculptureList/index.html
+- open the debugger
+- place a breakpoint at line 108
+- add \_useState[0] to the watch and run the code
+- watch how the array is used to set and then increment the index
+- notice how this extracts the next item from the sculptureList array
