31716ad0b0d423ca82894639e18e6896c46ef592
John Robertson <John.Robertson@nmtafe.wa.edu.au>
2024-07-16 15:24:53 +0800

    feat: challenge solutions session 1

--- session-01/assets/js_starter/scope.js
+++ session-01/assets/js_starter/scope.js
@@ -22,6 +22,3 @@ function myFunction() {
 }
 
 myFunction();
-
-
-FetchRequest
--- session-01/assets/js_starter/scripts_challenge_1.js
+++ session-01/assets/js_starter/scripts_challenge_1.js
@@ -2,9 +2,18 @@
 // Initialise the array
 let numbers = [1, 2, 3, 4, 5];
 
-// Define the function doubleNumbers
+// // Define the function doubleNumbers
+// function doubleNumbers(arr) {
+//     let doubled = arr.map((number) => number * 2);
+
+//     return doubled;
+// }
+
+let doubleNumbers = (arr) => {arr.map((number) => number * 2)}
+
 
 // Call the function with the numbers array
+let result =  doubleNumbers(numbers);
 
 // Console log the new array
 console.log(result);
