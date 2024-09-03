0d4a0854654d2bc6f06d5d20b43d91581f8aa77c
John Robertson <John.Robertson@nmtafe.wa.edu.au>
2024-09-03 14:16:34 +0800

    feat: template literals

--- session-08/in-class-code/challenges/challenge_1/template_literals.js
+++ session-08/in-class-code/challenges/challenge_1/template_literals.js
@@ -1,8 +1,25 @@
 // file: templateLiterals.js
+const userObject = {
+  firstName: "John",
+  lastName: "Doe",
+  age: 30,
+  isStudent: false,
+  address: {
+    street: "123 Main St",
+    city: "Perth",
+    zipCode: "6000",
+  },
+  skills: ["JavaScript", "HTML", "CSS"],
+  socialProfiles: {
+    twitter: "@johndoe",
+    linkedIn: "linkedin.com/in/johndoe",
+  },
+  isEmployed: true,
+};
 
 // The following greeting message uses template literals incorrectly.
 const getUserInfo = (user) => {
-  return "Name: ${user.firstName} ${user.lastName}, Age: ${user.age}";
+  return `Name: ${user.firstName} ${user.lastName}, Age: ${user.age}`;
 };
 
 // Fix the template literal syntax.
