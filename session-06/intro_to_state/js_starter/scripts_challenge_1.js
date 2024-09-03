5a2d5a108165f5754d7d0c240f6e55606d3ff009
Robbo-lab <john.robertson@nmtafe.wa.edu.au>
2024-09-03 16:38:26 +0800

    fix: revert main react classes

--- session-06/intro_to_state/js_starter/scripts_challenge_1.js
+++ session-06/intro_to_state/js_starter/scripts_challenge_1.js
@@ -1,22 +1,16 @@
 // Challenge - Stage 1
-let addBook = function (book) {
-    // If there's no book to do, do nothing
-    // data
-    if (!book || book.length < 1) return;
-    // Get the list
-    let list = document.querySelector('#app');
-    // Create a new list item
-    // template
-    let listItem = document.createElement('p');
-    // Append the item to the list
-    // render
-    list.appendChild(listItem)
-};
 
-let book = "Harry Potter IV"
+// If there's no book to do, do nothing
+// data
 
-addBook(book)
+// Get the list
 
+// Create a new list item
+// template
 
+// Append the item to the list
+// render
 
+let book = "Harry Potter IV";
 
+addBook(book);
