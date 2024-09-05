0643f8aefcdedf8e60af9e1d88cf22a1b2dacb55
Robbo-lab <john.robertson@nmtafe.wa.edu.au>
2024-09-05 14:22:14 +0800

    feat: session 9 slides, challenges and compoenent activity

---
+++ session-09/in-class-code/challenges/userObject.js
@@ -0,0 +1,71 @@
+const user = {
+  firstName: "John",
+  lastName: "Doe",
+  age: 30,
+  isStudent: false,
+  address: {
+    street: "123 Main St",
+    city: "Perth",
+    zipCode: "6000",
+  },
+  skills: ["JavaScript", "HTML", "CSS"],
+  socialProfiles: {
+    twitter: "@johndoe",
+    linkedIn: "linkedin.com/in/johndoe",
+  },
+  isEmployed: true,
+};
+
+const userList = [
+  {
+    firstName: "John",
+    lastName: "Doe",
+    age: 30,
+    isStudent: false,
+    address: {
+      street: "123 Main St",
+      city: "Perth",
+      zipCode: "6000",
+    },
+    skills: ["JavaScript", "HTML", "CSS"],
+    socialProfiles: {
+      twitter: "@johndoe",
+      linkedIn: "linkedin.com/in/johndoe",
+    },
+    isEmployed: true,
+  },
+  {
+    firstName: "Bianca",
+    lastName: "Doe",
+    age: 30,
+    isStudent: false,
+    address: {
+      street: "123 Main St",
+      city: "Perth",
+      zipCode: "6000",
+    },
+    skills: ["JavaScript", "HTML", "CSS"],
+    socialProfiles: {
+      twitter: "@biancadoe",
+      linkedIn: "linkedin.com/in/biancadoe",
+    },
+    isEmployed: true,
+  },
+  {
+    firstName: "Clare",
+    lastName: "Doe",
+    age: 30,
+    isStudent: false,
+    address: {
+      street: "123 Main St",
+      city: "Perth",
+      zipCode: "6000",
+    },
+    skills: ["JavaScript", "HTML", "CSS"],
+    socialProfiles: {
+      twitter: "@clairedoe",
+      linkedIn: "linkedin.com/in/clairedoe",
+    },
+    isEmployed: true,
+  },
+];
