60188e6344adcac0ce15683c7cee64371330101e
John Robertson <John.Robertson@nmtafe.wa.edu.au>
2025-07-21 14:06:14 +0800

    feat: update session 1 activity

--- session-01/assets/js_starter/scripts_challenge_1.js
+++ session-01/assets/js_starter/scripts_challenge_1.js
@@ -3,9 +3,16 @@
 let numbers = [1, 2, 3, 4, 5];
 
 // Define the function doubleNumbers
+// function doubleNumbers(arr) {
+//   let doubled = arr.map((number) => number * 2);
+
+//   return doubled;
+// }
+
+let doubledNumbers = (arr) => arr.map((number) => number * 2);
 
 // Call the function with the numbers array
-let result =  doubleNumbers(numbers);
+let result = doubleNumbers(numbers);
 
 // Console log the new array
 console.log(result);
