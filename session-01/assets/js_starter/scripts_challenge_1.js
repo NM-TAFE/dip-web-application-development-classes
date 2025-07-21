cb392b10eff2e06695798b342c7fd3140b7dd21e
John Robertson <John.Robertson@nmtafe.wa.edu.au>
2025-07-21 08:05:07 +0800

    test: git branch in class

--- session-01/assets/js_starter/scripts_challenge_1.js
+++ session-01/assets/js_starter/scripts_challenge_1.js
@@ -2,15 +2,7 @@
 // Initialise the array
 let numbers = [1, 2, 3, 4, 5];
 
-// // Define the function doubleNumbers
-// function doubleNumbers(arr) {
-//     let doubled = arr.map((number) => number * 2);
-
-//     return doubled;
-// }
-
-let doubleNumbers = (arr) => {arr.map((number) => number * 2)}
-
+// Define the function doubleNumbers
 
 // Call the function with the numbers array
 let result =  doubleNumbers(numbers);
