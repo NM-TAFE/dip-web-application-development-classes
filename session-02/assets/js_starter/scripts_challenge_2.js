a90f460fbeaba3eef73d0043b661c9e162f96885
Robbo-lab <john.robertson@nmtafe.wa.edu.au>
2025-07-28 10:17:36 +0800

    feat: object tidy up

--- session-02/assets/js_starter/scripts_challenge_2.js
+++ session-02/assets/js_starter/scripts_challenge_2.js
@@ -8,5 +8,5 @@ let array2 = [4, 5, 6, 4];
 // Use the Set object to remove duplicates
 
 // Use array destructuring to assign the first three elements to variables
-y;
+
 // Set the value of the output filed to be the second element of the unique array
