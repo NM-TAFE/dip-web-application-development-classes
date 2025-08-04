7b4d1125ce7dc91c8aa44b02b960eeb098683871
John Robertson <John.Robertson@nmtafe.wa.edu.au>
2025-08-04 11:02:31 +0800

    feat: session 2

--- session-02/assets/js_starter/scripts_challenge_1.js
+++ session-02/assets/js_starter/scripts_challenge_1.js
@@ -3,9 +3,14 @@
 let numbers = [1, 2, 3, 4, 5];
 
 // Use the pop() method to remove the last element from the array
+let lastElement = numbers.pop();
 
 // Use the unshift() method to add the last element to the beginning of the array
+numbers.unshift(lastElement);
 
 // Use the slice() method to exclude the first 3 elements and return a new array
+let newArray = numbers.slice(3);
 
 // Set the value of the output field inner html to the new array
+let outField = document.getElementById("Output field");
+outputField.innerHTML = newArray;
--- session-02/assets/js_starter/scripts_challenge_3.js
+++ session-02/assets/js_starter/scripts_challenge_3.js
@@ -1,12 +1,67 @@
 // Challenge 3 Starter
 // Initialise the array
-
+let users = [
+  {
+    firstName: "Aldus",
+    lastName: "Huxley",
+    age: 30,
+    isStudent: false,
+    address: {
+      street: "123 Main St",
+      city: "Perth",
+      zipCode: "6000",
+    },
+    skills: ["JavaScript", "HTML", "CSS"],
+    socialProfiles: {
+      twitter: "@aldus",
+      linkedIn: "linkedin.com/in/ahuxley",
+    },
+    isEmployed: true,
+  },
+  {
+    firstName: "John",
+    lastName: "Huxley",
+    age: 30,
+    isStudent: false,
+    address: {
+      street: "123 Main St",
+      city: "Perth",
+      zipCode: "6000",
+    },
+    skills: ["JavaScript", "HTML", "CSS"],
+    socialProfiles: {
+      twitter: "@aldus",
+      linkedIn: "linkedin.com/in/ahuxley",
+    },
+    isEmployed: true,
+  },
+  {
+    firstName: "Leif",
+    lastName: "Huxley",
+    age: 30,
+    isStudent: false,
+    address: {
+      street: "123 Main St",
+      city: "Perth",
+      zipCode: "6000",
+    },
+    skills: ["JavaScript", "HTML", "CSS"],
+    socialProfiles: {
+      twitter: "@aldus",
+      linkedIn: "linkedin.com/in/ahuxley",
+    },
+    isEmployed: true,
+  },
+];
 // Check if the array includes the number 5
 
-// If true, filter out numbers less than 5
-
-// Then square each number in the resulting array
-
-// Output the final array
-
-// Output a message if the number 5 is not in the array
+if (users) {
+  // If true, filter
+  let filteredUsers = users.filter((user) => user.skills[0] === "JavaScript");
+  let mappedUI = filteredUsers.map(
+    (user) => (document.getElementById("app").innerHTML = user.fristName)
+  );
+  // Then square each number in the resulting array
+  // Output the final array
+  // Output a message if the number 5 is not in the array
+}
