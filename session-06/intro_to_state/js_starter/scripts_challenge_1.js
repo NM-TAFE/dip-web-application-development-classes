3c3f62a24aacbaf55c4a650bc1776194b7f782fb
John Robertson <John.Robertson@nmtafe.wa.edu.au>
2025-08-25 12:15:45 +0800

    feat: up to challene 2

--- session-06/intro_to_state/js_starter/scripts_challenge_1.js
+++ session-06/intro_to_state/js_starter/scripts_challenge_1.js
@@ -1,16 +1,23 @@
 // Challenge - Stage 1
+let addBook = function (book) {
+  // If there's no book to do, do nothing
+  // data
+  if (!book || book.length < 1) return;
 
-// If there's no book to do, do nothing
-// data
+  // Get the list
+  // Create a new list item
+  let list = document.querySelector("#list");
 
-// Get the list
-
-// Create a new list item
-// template
-
-// Append the item to the list
-// render
+  // template
+  let listItem = document.createElement("li");
+  // Append the item to the list
+  listItem.textContent = book;
+  // render
+  list.appendChild(listItem);
+};
 
 let book = "Harry Potter IV";
+let book1 = "Harry Potter III";
 
 addBook(book);
+addBook(book1);
--- session-06/intro_to_state/js_starter/scripts_challenge_2.js
+++ session-06/intro_to_state/js_starter/scripts_challenge_2.js
@@ -1,11 +1,26 @@
-// Challenge - Stage 2
-// The data
+let data = {
+  books: ["Harry Potter I", "Harry Potter II", "Harry Potter III"],
+};
 
 // The template
+let template = function (props) {
+  let html =
+    "<ul>" +
+    props.books
+      .map(function (book) {
+        return "<li>" + book + "</li>";
+      })
+      .join("") +
+    "</ul>";
 
+  return html;
+};
 // Render the template into the UI
+let app = document.querySelector("#list");
 
 // Update the UI
 data.books.push("Harry Potter IV");
 data.books.push("Harry Potter V");
+// data.books.pop();
+// data.books[1] = "Harry Potter V";
 app.innerHTML = template(data);
