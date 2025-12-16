dceb309ca235dd65878ba077e435387ac4369e2b
John Robertson <John.Robertson@nmtafe.wa.edu.au>
2025-12-16 14:11:58 +0800

    chore: session 2 reset

--- session-02/assets/js_starter/scripts_challenge_4.js
+++ session-02/assets/js_starter/scripts_challenge_4.js
@@ -4,8 +4,10 @@ let numbers = [1, 2, 3, 4, 5, 6];
 
 // Find the index of the number 5 in the array
 
+
 // If the number 5 is found in the array
 
-// Console log each number in the array up to and including 5
+  // Console log each number in the array up to and including 5
 
-// Output a message if the number 5 is not in the array
+  // Output a message if the number 5 is not in the array
+  console.log("Number not in array")
