ecccef9eb0e3b7aeab270577caccbee7b3be80c8
John Robertson <John.Robertson@nmtafe.wa.edu.au>
2025-12-17 10:26:08 +0800

    feat: session 9 reset

--- session-03/assets/activity_1/math.js
+++
@@ -1,9 +0,0 @@
-export const add = function (a, b) {
-  return a + b;
-};
-
-export const subtract = (a, b) => a - b;
-
-export const divide = (a, b) => a / b;
-
-export default (a, b) => a * b;
--- session-09/src/App.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import DateInputComponent from "./components/DateInputComponent.js";
-
-function App() {
-  return <DateInputComponent />;
-}
-
-export default App;
