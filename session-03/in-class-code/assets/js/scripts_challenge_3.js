047f4ea6deeceb1cf1b0d788cf5c59aa944d742d
John Robertson <john@myjamjar.com.au>
2026-08-07 08:59:02 +0800

    feat: update session4

--- session-03/in-class-code/assets/js/scripts_challenge_3.js
+++ session-03/in-class-code/assets/js/scripts_challenge_3.js
@@ -1,51 +1,12 @@
 // Challenge 3 Starter
 // Create the class and use the constructor method
 class UserObject {
-  constructor(
-    firstName,
-    lastName,
-    age,
-    isStudent,
-    address,
-    skills,
-    socialProfiles,
-    isEmployed,
-    changeAge,
-    changeArrowAge,
-  ) {
-    this.firstName = firstName;
-    this.lastName = lastName;
-    this.age = age;
-    this.isStudent = isStudent;
-    this.address = address;
-    this.skills = skills;
-    this.socialProfiles = socialProfiles;
-    this.isEmployed = isEmployed;
-    this.changeAge = changeAge;
-    this.changeArrowAge = changeArrowAge;
-  }
+  //
 }
 
 // Create an instance of the ComplexObject class (hint: add the properties)
-const user1 = new UserObject(
-  "Aldus",
-  "Huxley",
-  30,
-  false,
-  "123 Main St Perth 6000",
-  ["JavaScript", "HTML", "CSS"],
-  {
-    twitter: "@aldus",
-    linkedIn: "linkedin.com/in/ahuxley",
-  },
-  true,
-  function () {
-    return (this.age += 1);
-  },
-  () => {
-    this.age += 1;
-  },
-);
+const user1 = new UserObject();
+//
 
 // Log the new instance of the object
 console.log(user1);
