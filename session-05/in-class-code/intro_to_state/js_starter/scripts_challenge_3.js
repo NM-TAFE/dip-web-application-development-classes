eb88ab08384dc1dfcb0be384b1efd1277972db3c
John Robertson <ROBERJ@markonetechnologies.com.au>
2026-08-20 08:32:12 +0800

    feat: session 5

--- session-05/in-class-code/intro_to_state/js_starter/scripts_challenge_3.js
+++ session-05/in-class-code/intro_to_state/js_starter/scripts_challenge_3.js
@@ -1,18 +1,1 @@
 // Challenge 3 Book constructor
-function Book(title, author) {
-  this.title = title;
-  this.author = author;
-  // Method to display book information
-
-  this.displayInfo = function () {
-    console.log(`${this.title}, author: ${this.author}`);
-  };
-}
-
-// Creating instances using the Book constructor
-let book1 = new Book("Harry Potter I", "J.K. Rowling");
-let book2 = new Book("Harry Potter II", "J.K. Rowling");
-
-// Displaying book information
-book1.displayInfo();
-book1.displayInfo();
