39b555d85ce911f8a9e7389509ed23ceb74507ec
John Robertson <John.Robertson@nmtafe.wa.edu.au>
2026-08-20 13:53:43 +0800

    feat: session 5 example

--- session-05/in-class-code/intro_to_state/js_starter/scripts.js
+++ session-05/in-class-code/intro_to_state/js_starter/scripts.js
@@ -14,21 +14,22 @@ let app = new UserComponent("#app", {
   data,
   template: (props) => {
     let html = props.map(
-      (user) => `
-                    <div class="user-card">
-                    <h2>${user.firstName} ${user.lastName}</h2>
-                    <p><strong>Age:</strong> ${user.age}</p>
-                    <p><strong>Employment:</strong> ${
-                      user.isEmployed ? "Employed" : "Not Employed"
-                    }</p>
-                    <p><strong>Location:</strong> ${user.address.city}</p>
-                    <p><strong>Skills:</strong> ${user.skills.join(", ")}</p>
-                    <div class="social-links">
-                        #<br>
-                        #LinkedIn</a>
-                    </div>
-                    </div>
-                `,
+      (user) =>
+        `
+            <div class="user-card">
+                <h2>${user.firstName} ${user.lastName}</h2>
+                <p><strong>Age:</strong> ${user.age}</p>
+                <p><strong>Employment:</strong> ${
+                  user.isEmployed ? "Employed" : "Not Employed"
+                }</p>
+                <p><strong>Location:</strong> ${user.address.city}</p>
+                <p><strong>Skills:</strong> ${user.skills.join(", ")}</p>
+                <div class="social-links">
+                    #<br>
+                    #LinkedIn</a>
+                </div>
+            </div>
+        `,
     );
 
     return html;
