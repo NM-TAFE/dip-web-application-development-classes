796ff5067c498989a3c6d26fbaad0d9f648a4dc2
John Robertson <ROBERJ@markonetechnologies.com.au>
2026-08-27 08:10:35 +0800

    feat: session 4 & 5 update

--- session-04/in-class-code/nasa-api-app/js/app.js
+++ session-04/in-class-code/nasa-api-app/js/app.js
@@ -24,18 +24,55 @@ document.addEventListener("DOMContentLoaded", () => {
     }
     // console.log(params);
 
-    // Challenge 2
-    // 1. add the following headers to your API call content type, user agent & cache control
-    // 2. set the cache control no no cache
-    // 3. set the content type application/json
-    // 4. Test the responses in the Network tab
-
-    // Challenge 3
-
-    //   // Challenge 4
-    //   // 1. Move the if/else if/else into a resusable function ready for modularisation
-    // }
-
-    // Challenge 4: Modularise display logic
+    fetch(apiUrl, {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+        "User-Agent": "APODViewer/1.0",
+        "Cache-Control": "no-cache",
+      },
+    })
+      // Challenge 3
+      // 1. change the anonymous arrow function below to check if the response code is 200(ok)
+      // 2. if the response is ok return the response.json() object
+      // 3. if not ok throw a new error which includes the status code
+      // 4. Test the responses in the Network tab
+      .then(async (response) => {
+        if (response.ok) {
+          return response.json();
+        }
+        throw new Error("The API call failed");
+      })
+      .then((data) => {
+        if (Array.isArray(data)) {
+          apodContent.innerHTML = "";
+          data.map((item) => {
+            displayTernaryApodItem(item, apodContent);
+          });
+        } else {
+          displayTernaryApodItem(item, apodContent);
+        }
+      })
+      .catch((error) => {
+        apodContent.innerHTML += `<p>Media type not supported: ${item.media_type}</p> - ${error}`;
+      });
   });
 });
+
+function displayTernaryApodItem(item, container) {
+  item.media_type === "image"
+    ? (container.innerHTML += `
+          <figure class="image is-4by3">
+            <img src="${item.url}" alt="${item.title}">
+          </figure>
+          <h2 class="title is-4">${item.title}</h2>
+          <p>${item.explanation}</p>
+        `)
+    : (container.innerHTML += `
+          <div class="video-apodContent">
+            <iframe src="${item.url}" frameborder="0" allowfullscreen></iframe>
+          </div>
+          <h2 class="title is-4">${item.title}</h2>
+          <p>${item.explanation}</p>
+        `);
+}
